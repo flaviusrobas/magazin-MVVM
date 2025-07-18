410b6d8bd4e9e9564a34d0da4cac56d607fcaa54
flavius robas <flaviusrobas@yahoo.ro>
2025-07-18 20:14:45 +0300

    Add project files.

---
+++ Magazin/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Magazin.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Magazin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Magazin.WebApiApplication" Language="C#" %>
